fd56dbef6725d9cfbde6a74276e1b94c81224dd5
Chandra Shekhar <csahu3932@gmail.com>
2020-01-27 19:38:13 +0530

    stduent can now view aluminis

---
+++ student/show_alumni_college.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <script  src='../jquery-3-4-1.min.js'></script>
+    <title></title>
+</head>
+<body>
+<?php
+session_start();
+$_SESSION['user']='student2';
+$con=mysqli_connect('localhost','root','','alumniconnect');
+$query = "select college from student where deleted=0 and username='$_SESSION[user]' ";
+$rs_student = mysqli_query($con,$query);
+$temp=0;
+echo'<table>
+<thead>
+<th>Sno.</th><th>Name</th><th>College Name</th> <th> branch </th> <th> Email </th><th> Contact </th><th> Passing Year </th><th> Designation</th>
+</thead>
+<tbody>';
+while($row= mysqli_fetch_array($rs_student) )
+{
+    $query2="select * from alumni_detail where college = '$row[college]'";
+    $res_alumni=mysqli_query($con,$query2);
+    while($row_alumni = mysqli_fetch_array($res_alumni))
+        {
+            $temp++;
+            echo"<tr><th>$row_alumni[id]</th><th>$row_alumni[name]</th><th>$row_alumni[college]</th><th>$row_alumni[branch]</th><th>$row_alumni[email]</th><th>$row_alumni[contact]</th><th>$row_alumni[passing_year]</th><th>$row_alumni[designation]</th></tr>";
+        }
+}
+echo"</tbody>";
+echo"</table>";
+?> 
+
+</body>
+</html>
