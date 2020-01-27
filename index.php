a35330a0cdaf0996815169bc48de3de828d70b05
arunKumar <you@example.com>
2020-01-27 16:42:34 +0530

    index

--- index.php
+++ index.php
@@ -18,14 +18,59 @@
             <a href="index.php">Home</a>
             <a href="login.php">Login</a>
         </div>
-        <p class="detail">Get Connected__ALUMINI</p>
+        <p class="detail">Get Connected__ALUMNI</p>
         <img src="assets/images/" alt="">
 
         <a href=""><i class="fas fa-chevron-down fa-4x"></i></a>
     </header>
 
     <section class = "section">
-    
+        <div class="section1">
+             <!-- <img src="https://images.unsplash.com/photo-1561489413-985b06da5bee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""> -->
+             <!-- <h3>Meetups</h3> -->
+        </div>
+        <div class="section2">
+          <!-- <img src="assets/images/group.png" alt=""> -->
+          <h3>Meetups</h3><br><br><br>          <p>The sole purpose of the  once part of the institute. When they come to the alumni meet, people of different year be it junior or senior they just interact with each other and network.
+
+Many of them have become influential workingThe <br> sole purpose of the alumni meet is to get together all of the people who were once part of the institute. When they come to the alumni meet, people of different year be it junior or senior they just interact with each other and network.
+
+Many of them have become influential working professionals​ which can help out the current students at the institute as well as their batch mates. professionals​ which can help out the current students at the institute as well as their batch mates.</p>
+        </div>
+        <div class="section3">
+             <!-- <img src="assets/images/notices.png" alt=""> -->
+             <h3>Know Alumini Status</h3><br><br><br>
+             <p>Know your alThe sole purpose of the alumni meet is to get together all of the people who were once part of the institute. When they come to the alumni meet, people of different year be it junior or senior they just interact with each other and network.
+
+Many of them have become influential working professionals​.</p><br><p> which can help out the current students at the institute as well as their batch mates.The sole purpose of the alumni meet is to get together all of the people who were once part of the institute. When they come to the alumni meet, people of different year be it junior or senior they just interact with each other and network.
+
+Many of them have become influential working professionals​ which can help out the current students at the institute as well as their batch mates.</p>
+        </div>
+        <div class="section4">
+        <!-- <h3>Events</h3> -->
+            <!-- <img src="assets/images/status.png" alt="" class="sec-img"> -->
+<!--           
+        </div>
+        <div class="section5">
+          <img src="assets/images/events.png" alt="">
+          <h3>Events</h3>
+        </div>
+        <div class="section6">
+            <div class="img1">
+
+            </div>
+            <div class="img2">
+
+            </div>            
+        </div>
+        <div class="section7">
+            <div class="img1">
+
+            </div>
+            <div class="img2">
+                
+            </div>             -->
+        <!-- </div> -->
     </section>
 
     <footer class="footer">
