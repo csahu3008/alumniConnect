2237e38f5a79226a0ea93abd70fa4b024ec93b8d
arunKumar <you@example.com>
2020-01-27 17:55:51 +0530

    index update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Demo-Home Page</title>
+    <title>ALUMS</title>
     <link rel="stylesheet" href="assets/css/index.css">
     <link rel = "stylesheet" href = "assets/css/fontawesome/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|Righteous|Varela+Round&display=swap" rel="stylesheet">
@@ -12,7 +12,7 @@
 <body>
     <header class="header">
         <div class="nav">
-            <p class="web-name">Demo</p>
+            <p class="web-name">ALUMS</p>
         </div>
         <div class="nav2">
             <a href="index.php">Home</a>
@@ -52,21 +52,28 @@ Many of them have become influential working professionals​ which can help out
           <h3>Events</h3>
         </div>
         <div class="section6">
-            <div class="img1">
-
-            </div>
-            <div class="img2">
-
+            <div class="img1"></div>
+            <div class="img2"><br><br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae non quo suscipit provident voluptatibus temporibus aut tenetur quibusdam, nemo quos exercitationem quaerat vel maiores veritatis impedit accusantium totam ex in!Suscipit animi voluptatum distinctio error quod dolore natus quidem quae<br> eveniet ullam enim aut eos delectus quam excepturi, itaque sapiente, quia quas tenetur dolorem impedit veniam! Culpa, ipsa. Velit, id.</p>
             </div>            
         </div>
         <div class="section7">
-            <div class="img1">
-
-            </div>
-            <div class="img2">
-                
-            </div>             -
+            <div class="img3"></div>
+            <div class="img2"><br><br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ipsam quidem alias libero quo? Ullam <br>incidunt ea culpa laborum fugit libero rerum delectus id at veritatis, magni nostrum voluptate dolor.Quia ullam saepe hic illum ea provident eligendi eum harum exercitationem nam sequi beatae, commodi numquam consequatur repellendus, voluptatibus, cum quae facere non. Tempore voluptate consequuntur libero pariatur illo id?</p>
+            </div>             
          </div> 
+         <div class="section8"></div>
+        <div class="section9">
+          <!-- <img src="assets/images/group.png" alt=""> -->
+          <h3>Group Chat</h3><br><br><br>         
+           <p>The sole purpose of the  once part of the institute. When they come to the alumni meet, people of different year be it junior or senior they just interact with each other and network.
+
+         <br> sole purpose of the alumni meet is to get together all of the people who were once part of the institute. When they come to the alumni meet, people of different year be it junior or senior they just interact with each other and network.
+
+            Many of them have become influential working professionals​ which can help out the current students at the institute as well as their batch mates. professionals​ which can help out the current students at the institute as well as their batch mates.</p>
+        </div>
+
     </section>
 
     <footer class="footer">
