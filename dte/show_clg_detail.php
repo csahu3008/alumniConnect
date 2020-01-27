404d3c2553a85ce92c3d3458ad29dee9caff6b3f
Vishwas Paikra <vishwaspaikra007@gmail.com>
2020-01-27 19:29:05 +0530

    dte partial design

--- dte/show_clg_detail.php
+++ dte/show_clg_detail.php
@@ -5,21 +5,79 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        } html {
+            min-width: 700px;
+        }
+        h1{
+            background: cadetblue;
+            padding: 20px;
+            border-radius: 0 0 40px 40px;
+            text-align: center;
+        }
+        .listHeading, .collegeContainer {
+            display: grid;
+            grid-template-columns: 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5%;
+            padding: 10px;
+        }.listHeading > div, .collegeContainer > div {
+            margin: 10px;
+            padding: 5px;
+            background: #ffffffaa;
+            border-radius: 10px;
+            overflow: hidden;
+            overflow-x: auto;
+        } .headContainer {
+            background: darkcyan;
+        } .collegeContainer {
+            border-bottom: 2px dashed blue;
+        } .listContainer {
+            box-shadow: 0 -3px 15px -4px;
+        }
+
+    div::-webkit-scrollbar {
+        width: 0px;
+        height: 0;
+    }
+    
+    div::-webkit-scrollbar-track {
+        background: #00000000; 
+    }
+    
+    /* Handle */
+    div::-webkit-scrollbar-thumb {
+        background: rgba(0, 0, 0, 0.521); 
+        border-radius: 0px;
+    }
+    
+    /* Handle on hover */
+    div::-webkit-scrollbar-thumb:hover {
+        background: rgba(255, 255, 255, 0.87); 
+    } 
+    </style>
 </head>
 <body>
 
     <?php
+
+    echo'<div class="headContainer">
+    <h1 class="heading">List of Colleges</h1>';
+
 
     include('../connect.php');
     $cId = $_REQUEST['cId'];
     
     //echo '<div class="table"></div>';
     $temp = 0;
-    echo'<table>
-         <thead>
-         <th>Sno.</th><th>College Name</th> <th> Category </th> <th> Email </th><th> Contact </th><th> Pincode </th><th> Address </th><th>Show Detail </th>
-         </thead>
-         <tbody>';
+    echo'
+         <div class="listHeading"><div>index</div><div>College Name</div> <div> Category </div> <div> Email </div><div> Contact </div><div> Pincode </div><div> Address </div><div>Show Detail </div>
+        ';
+
+    echo'</div></div>';
+    echo'<div class="listContainer">';
 
 
         $query = "select * from college where deleted=0 and id=$cId";
